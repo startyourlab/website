6ec41946ef2197a841c0acd8cf7e40f6a601d8ed
Ari Dyckovsky <aridyckovsky@gmail.com>
2021-04-07 19:51:02 -0600

    Begin organizing sections

--- sidebars.js
+++ sidebars.js
@@ -2,14 +2,9 @@ module.exports = {
   docs: [
     {
       type: 'category',
-      label: 'Docusaurus Tutorial',
+      label: 'Digital Collaboration',
       items: [
-        'getting-started',
-        'create-a-page',
-        'create-a-document',
-        'create-a-blog-post',
-        'markdown-features',
-        'thank-you',
+        'get-started',
       ],
     },
   ],
